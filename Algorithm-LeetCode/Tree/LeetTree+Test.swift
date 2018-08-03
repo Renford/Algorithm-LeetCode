d59ed7d42943cfef99d5d130d72ea9f645371a08
断虹残雪 <chaofree3088@sina.com>
2018-08-04 07:11:57 +0800

    [Add] add tree algorithm

---
+++ Algorithm-LeetCode/Tree/LeetTree+Test.swift
@@ -0,0 +1,9 @@
+//
+//  LeetTree+Test.swift
+//  Algorithm-LeetCode
+//
+//  Created by Renford on 2018/8/2.
+//  Copyright © 2018年 Renford. All rights reserved.
+//
+
+import Foundation
---
+++ Algorithm-LeetCode/Tree/LeetTree.swift
@@ -0,0 +1,28 @@
+//
+//  LeetTree.swift
+//  Algorithm-LeetCode
+//
+//  Created by Renford on 2018/8/2.
+//  Copyright © 2018年 Renford. All rights reserved.
+//
+
+import UIKit
+
+public class TreeNode {
+     public var val: Int
+     public var left: TreeNode?
+     public var right: TreeNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.left = nil
+         self.right = nil
+     }
+}
+
+class LeetTree: NSObject {
+
+}
+
+extension LeetTree {
+    
+}
