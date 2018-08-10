f1f569c4a1a72957aab64f48b8b64e13aa7584ef
RenChao <renchao@emoney.cn>
2018-08-10 08:48:21 +0800

    [Add] add LFU for cache algorithm

---
+++ Algorithm-LeetCode/Cache/LeetCache+Test.swift
@@ -0,0 +1,13 @@
+//
+//  LeetCache+Test.swift
+//  Algorithm-LeetCode
+//
+//  Created by RenChao on 2018/8/10.
+//  Copyright © 2018年 Renford. All rights reserved.
+//
+
+import Foundation
+
+extension LeetCache {
+    
+}
---
+++ Algorithm-LeetCode/Cache/LeetCache.swift
@@ -0,0 +1,15 @@
+//
+//  LeetCache.swift
+//  Algorithm-LeetCode
+//
+//  Created by RenChao on 2018/8/10.
+//  Copyright © 2018年 Renford. All rights reserved.
+//
+
+import Foundation
+
+/// 460. LFU缓存
+/// https://leetcode-cn.com/problems/lfu-cache/description/
+class LeetCache: NSObject {
+
+}
